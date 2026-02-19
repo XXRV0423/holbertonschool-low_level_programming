6fc4fdc9282e6156ae8b131380faa8b303c3f375
XXRV0423 <xxavierrv@gmail.com>
2026-02-18 20:07:08 -0400

    Function that prints the numbers from 0 to 9 excluding the numbers 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the number from 0 to 9, excluding 2 and 4
+ *
+ * You can only use _putchar twice
+ */
+
+void print_moat_numbers(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		if (c != '2' && c != '4')
+			_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
