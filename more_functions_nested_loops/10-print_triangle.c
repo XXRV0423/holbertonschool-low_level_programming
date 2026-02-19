8060ffa3cff6969cdad1d84db5426c5c491631f1
XXRV0423 <xxavierrv@gmail.com>
2026-02-18 23:36:34 -0400

    Function that prints a triangle, followed by a new line

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a right-aligned triangle
+ * @size: size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int row, space, hash;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (row = 1; row <= size; row++)
+	{
+		/*print leading spaces */
+		for (space = 0; space < size - row; space++)
+			_putchar(' ');
+
+		/*print hashes*/
+		for (hash = 0; hash < row; hash++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+}
