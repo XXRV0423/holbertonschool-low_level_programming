4adcdac33f9bfb1135c866dd8a2683072339f4ed
XXRV0423 <xxavierrv@gmail.com>
2026-02-26 18:53:11 -0500

    Function that returns the factorial of a given number

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -6,17 +6,12 @@
  *
  * Return: factorial of n, or -1 if n is negative
  */
- int factorial(int n)
- {
-    if (n < 0) /*Error case*/
-    { 
-        return (-1);
-    }
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
 
-    if (n == 0) /*Base case*/
-    { 
-        return (1);
-    }
-
-    return (n * factorial(n - 1)); /*Recursive call*/
- }
+	return (n * factorial(n - 1));
+}
