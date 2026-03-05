3c860f9b71a8219643daa8b5452352c12c9cad78
XXRV0423 <xxavierrv@gmail.com>
2026-03-05 18:23:55 -0400

    Structure for dog.h added

---
+++ structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - Structure that represents a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
