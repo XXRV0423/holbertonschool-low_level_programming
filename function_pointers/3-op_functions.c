ecc12befa1bf5898fbacd6bd33ea3d5cbf8d7832
XXRV0423 <xxavierrv@gmail.com>
2026-03-10 19:51:27 -0400

    File with all the functions for performing mathemathical operations

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ * @a: first integer (number)
+ * @b: second integer (number)
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substracts two integers
+ * @a: first integer (number)
+ * @b: second integer (number)
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two integers
+ * @a: first integer (number)
+ * @b: second integer (number)
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two integers
+ * @a: first integer (number)
+ * @b: second integer (number)
+ * Return: result of division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo of two integers
+ * @a: first integer (number)
+ * @b: second integer (number)
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
