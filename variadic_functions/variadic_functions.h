f1d88aa340cb4c68d000628830ea5c98706e7d04
XXRV0423 <xxavierrv@gmail.com>
2026-03-14 15:17:00 -0400

    Prototype for print_all.c fixed

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,18 @@
-#ifndef VARIADICT_FUNCTIONS_H
-#define VARIADICT_FUNCTIONS_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
 #include <stdarg.h>
+
+/**
+ * struct token - format specifier and function
+ * @token: format character
+ * @f: function pointer
+ */
+typedef struct token
+{
+	char *token;
+	void (*f)(va_list);
+} token_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
