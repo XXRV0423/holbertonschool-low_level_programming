927c34b01ac313d871dda17ddd8b2c11c385ef00
XXRV0423 <xxavierrv@gmail.com>
2026-03-26 17:32:01 -0400

    Betty style fixed

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: number of nodes
+ */
+
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
