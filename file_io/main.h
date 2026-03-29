c9bbce670ed048023ae81b716d4db57294eda620
XXRV0423 <xxavierrv@gmail.com>
2026-03-29 19:04:12 -0400

    Main header file with the prototype function of read_textfile.c

---
+++ file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+#include <sys/types.h>
+
+/*Prototype functions*/
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif 
