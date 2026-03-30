3eb81aa74783d2e1683a91485564f4de69b966be
XXRV0423 <xxavierrv@gmail.com>
2026-03-29 20:00:03 -0400

    Function that creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,10 +16,10 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	while (text_contetn && text_content[len])
+	while (text_content && text_content[len])
 		len++;
 
-	fd = open(filenae, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
